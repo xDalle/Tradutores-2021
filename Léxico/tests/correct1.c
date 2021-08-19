dd84563c12e4a36f767a352cf6aaa2c3ec2d9451
Lucas Dalle <lucasdalle@live.com>
2021-08-19 17:36:19 -0300

    Add files via upload

---
+++ Léxico/tests/correct1.c
@@ -0,0 +1,17 @@
+int main(){
+    int a, b;
+    float c;
+    read(a);
+    writeln(a);
+    /*	MULTICOMMENT
+          IGNORED
+                    */
+    c = -99.9E+14;
+    b = -8;
+    if(a == b){
+        write("Bye!");
+    }else{
+        c = a/b;	//Also ignored!
+    }
+return 0;
+}
---
+++ Léxico/tests/incorrect1.c
@@ -0,0 +1,14 @@
+int main(){
+    int a, $b;
+    float \c;
+    c = 99.9.8
+    read(a);
+    writeln(a);
+	/*	MULTICOMMENT
+		IGNORED
+							
+    write("Bye!");
+	c = a/b;	//Also ignored!
+	}
+return 0;
+}
